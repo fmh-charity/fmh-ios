c3e481bd97188e0ea89d2388c454eecc3e69536d
Konstantin Tugolukov <031184@bk.ru>
2022-07-11 21:59:38 +0300

    scene add in mainScreen

--- fmh/Application/SceneDelegate.swift
+++ fmh/Application/SceneDelegate.swift
@@ -23,11 +23,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             appCoordinator = AppCoordinator(window: window)
-            appCoordinator?.start()
+            //appCoordinator?.start()
             self.window = window
             window.makeKeyAndVisible()
         }
 
+        let factory = ModuleFactory()
+        let vc = factory.makeMainScreenViewController()
+        window?.rootViewController = vc.toPresent
+        window?.makeKeyAndVisible()
+        
         func sceneDidDisconnect(_ scene: UIScene) {
             // Called as the scene is being released by the system.
             // This occurs shortly after the scene enters the background, or when its session is discarded.
