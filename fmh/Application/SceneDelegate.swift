967198e735335123f69e73dec4b7b33c1e829a3b
Konstantin Tugolukov <031184@bk.ru>
2022-12-18 13:24:00 +0300

    продолжаю очистку

--- fmh/Application/SceneDelegate.swift
+++ fmh/Application/SceneDelegate.swift
@@ -11,22 +11,13 @@ import Combine
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
-    var appCoordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
-        //AppSession.logOut()
-        //AppSession.tokens = TokenData(accessToken: "", refreshToken: AppSession.tokens!.refreshToken)
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            appCoordinator = AppCoordinator(window: window)
-            appCoordinator?.start()
-            self.window = window
-            window.makeKeyAndVisible()
-        }
+        
 
         func sceneDidDisconnect(_ scene: UIScene) {
             // Called as the scene is being released by the system.
